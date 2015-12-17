73e1414f7ba4116965600ca969d3de96529f784d
Lauren Grant <lgrant1286@gmail.com>
2015-12-17 14:02:55 -0500

    Correct image filepath for uploads.
    * Allows unlimited images to be uploaded to AWS.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :item_image, styles: { item_feature: "280x280>", item_dashboard: "222x222>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :item_image, styles: { item_feature: "280x280>", item_dashboard: "222x222>" }, default_url: "/images/:style/missing.png",
+    :path => "/images/:id/:style/:filename"
   validates_attachment_content_type :item_image, content_type: /\Aimage\/.*\Z/
 
   def rename_item_image
